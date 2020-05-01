262887cc9e675cf2c849f720606ae4d5381cea08
Ivan Tarasov <guser999@mail.ru>
2020-05-01 20:25:37 +0700

    refactor(index.js) Сделал вывод содержимого файлов

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 #!/usr/bin/env node
 import { program } from 'commander';
+import path from 'path';
+import fs from 'fs';
 
 program
   .version('0.0.1')
@@ -8,3 +10,13 @@ program
   .arguments('<firstConfig> <secondConfig>');
 
 program.parse(process.argv);
+
+const currentPath = process.cwd();
+const [, , file1, file2] = process.argv;
+const path1 = path.join(currentPath, file1);
+const path2 = path.join(currentPath, file2);
+
+const content1 = JSON.parse(fs.readFileSync(path1));
+const content2 = JSON.parse(fs.readFileSync(path2));
+
+console.log(content1, content2);
