3be9e4d25c08f4b7666994605d5aa614b877e0ea
Ivan Tarasov <guser999@mail.ru>
2020-05-03 16:52:36 +0700

    feat(index.js) Сделал вывод различий

--- src/index.js
+++ src/index.js
@@ -16,7 +16,34 @@ const [, , file1, file2] = process.argv;
 const path1 = path.join(currentPath, file1);
 const path2 = path.join(currentPath, file2);
 
-const content1 = JSON.parse(fs.readFileSync(path1));
-const content2 = JSON.parse(fs.readFileSync(path2));
+const obj1 = JSON.parse(fs.readFileSync(path1, 'utf8'));
+const obj2 = JSON.parse(fs.readFileSync(path2, 'utf8'));
 
-console.log(content1, content2);
+const keys1 = Object.keys(obj1);
+const keys2 = Object.keys(obj2);
+
+let diff = '';
+
+for (const key of keys1) {
+  const isAfter = keys2.includes(key);
+  const beforeValue = obj1[key];
+  const afterValue = obj2[key];
+
+  if (isAfter) {
+    if (beforeValue === afterValue) {
+      diff = diff.concat(`  ${key}: ${beforeValue}\n`);
+    } else {
+      diff = diff.concat(`- ${key}: ${beforeValue}\n+ ${key}: ${afterValue}\n`);
+    }
+  } else {
+    diff = diff.concat(`- ${key}: ${beforeValue}\n`);
+  }
+}
+
+for (const key of keys2) {
+  if (keys1.includes(key) === false) {
+    diff = diff.concat(`+ ${key}: ${obj2[key]}\n`);
+  }
+}
+
+console.log(diff);
