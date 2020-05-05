17d39b26bb7fcb220d5b8af3d5dfd6d5cdb78d4c
Ivan Tarasov <guser999@mail.ru>
2020-05-05 19:50:13 +0700

    feat(test) Добавил фикстуры, перевёл тесты на работу с ними

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,8 +1,13 @@
+import path from 'path';
 import genDiff from '../src/gendiff';
 
 test('genDiff', () => {
-  const path1 = '/home/tim/my_project/testfile/before.json';
-  const path2 = '/home/tim/my_project/testfile/after.json';
+  const pwd = process.cwd();
+  const currentPath1 = 'fixtures/before.json';
+  const currentPath2 = 'fixtures/after.json';
+  const path1 = path.join(pwd, currentPath1);
+  const path2 = path.join(pwd, currentPath2);
+
   const result = '  host: hexlet.io\n- timeout: 50\n+ timeout: 20\n- proxy: 123.234.53.22\n- follow: false\n+ verbose: true\n';
   expect(genDiff(path1, path2)).toBe(result);
 });
