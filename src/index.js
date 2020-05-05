e4f85a04c85ef2a49741db61d8d730e2f266100c
Ivan Tarasov <guser999@mail.ru>
2020-05-05 12:55:20 +0700

    refactor(package.json) Сделал чтобы работа шла через lodash

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@
 import { program } from 'commander';
 import path from 'path';
 import fs from 'fs';
+import _ from 'lodash';
 
 program
   .version('0.0.1')
@@ -16,33 +17,31 @@ const [, , file1, file2] = process.argv;
 const path1 = path.join(currentPath, file1);
 const path2 = path.join(currentPath, file2);
 
-const obj1 = JSON.parse(fs.readFileSync(path1, 'utf8'));
-const obj2 = JSON.parse(fs.readFileSync(path2, 'utf8'));
-
-const keys1 = Object.keys(obj1);
-const keys2 = Object.keys(obj2);
+const before = JSON.parse(fs.readFileSync(path1, 'utf8'));
+const after = JSON.parse(fs.readFileSync(path2, 'utf8'));
 
 let diff = '';
 
-for (const key of keys1) {
-  const isAfter = keys2.includes(key);
-  const beforeValue = obj1[key];
-  const afterValue = obj2[key];
+const keys = new Set([...Object.keys(before), ...Object.keys(after)]);
 
-  if (isAfter) {
+for (const key of keys) {
+  const isBefore = _.has(before, key);
+  const isAfter = _.has(after, key);
+  const beforeValue = before[key];
+  const afterValue = after[key];
+
+  if (isBefore && isAfter) {
     if (beforeValue === afterValue) {
       diff = diff.concat(`  ${key}: ${beforeValue}\n`);
     } else {
       diff = diff.concat(`- ${key}: ${beforeValue}\n+ ${key}: ${afterValue}\n`);
     }
   } else {
-    diff = diff.concat(`- ${key}: ${beforeValue}\n`);
-  }
-}
-
-for (const key of keys2) {
-  if (keys1.includes(key) === false) {
-    diff = diff.concat(`+ ${key}: ${obj2[key]}\n`);
+    if (isBefore) {
+      diff = diff.concat(`- ${key}: ${beforeValue}\n`);
+    } else {
+      diff = diff.concat(`+ ${key}: ${afterValue}\n`);
+    }
   }
 }
 
