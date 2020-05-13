001ff5bd5585035caf9be6262c759258182710e6
Web-proger <guser999@mail.ru>
2020-05-14 01:09:46 +0700

    test(gendiff.test.js) Дополнил тесты для yml файлов и yml + json

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -4,11 +4,28 @@ import genDiff from '../src/gendiff';
 
 test('genDiff', () => {
   const pwd = process.cwd();
-  const currentPath1 = 'fixtures/before.json';
-  const currentPath2 = 'fixtures/after.json';
-  const path1 = path.join(pwd, currentPath1);
-  const path2 = path.join(pwd, currentPath2);
+  let currentPath1 = 'fixtures/json/before.json';
+  let currentPath2 = 'fixtures/json/after.json';
 
-  const result = '  host: hexlet.io\n- timeout: 50\n+ timeout: 20\n- proxy: 123.234.53.22\n- follow: false\n+ verbose: true\n';
-  expect(genDiff(path1, path2)).toBe(result);
+  let path1 = path.join(pwd, currentPath1);
+  let path2 = path.join(pwd, currentPath2);
+
+  const resultFromJson = '  host: hexlet.io\n- timeout: 50\n+ timeout: 20\n- proxy: 123.234.53.22\n- follow: false\n+ verbose: true\n';
+  expect(genDiff(path1, path2)).toBe(resultFromJson);
+
+  currentPath1 = 'fixtures/yml/before.yml';
+  currentPath2 = 'fixtures/yml/after.yml';
+  path1 = path.join(pwd, currentPath1);
+  path2 = path.join(pwd, currentPath2);
+
+  const resultFromYml = '  format: yml\n- timeout: 100\n+ timeout: 120\n- revision: v1\n+ revision: v2\n+ step: 5\n';
+  expect(genDiff(path1, path2)).toBe(resultFromYml);
+
+  currentPath1 = 'fixtures/yml/before.yml';
+  currentPath2 = 'fixtures/json/after.json';
+  path1 = path.join(pwd, currentPath1);
+  path2 = path.join(pwd, currentPath2);
+
+  const resultYmlJson = '- format: yml\n- timeout: 100\n+ timeout: 20\n- revision: v1\n+ verbose: true\n+ host: hexlet.io\n';
+  expect(genDiff(path1, path2)).toBe(resultYmlJson);
 });
