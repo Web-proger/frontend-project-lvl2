a562f89e3521bafedaeb8f086e53847f8ff0a4a8
Ivan Tarasov <guser999@mail.ru>
2020-05-16 13:55:56 +0700

    test(gendiff.test.js) refactor test

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -3,51 +3,43 @@ import path from 'path';
 import genDiff from '../src/gendiff';
 
 let pwd;
-let currentPath1;
-let currentPath2;
-let path1;
-let path2;
+let before;
+let after;
+
+const getPath = (currentPath) => path.join(pwd, currentPath);
 
 beforeAll(() => {
   pwd = process.cwd();
 });
 
 test('JSON', () => {
-  currentPath1 = 'fixtures/json/before.json';
-  currentPath2 = 'fixtures/json/after.json';
-  path1 = path.join(pwd, currentPath1);
-  path2 = path.join(pwd, currentPath2);
+  before = 'fixtures/json/before.json';
+  after = 'fixtures/json/after.json';
 
   const resultFromJson = '  host: hexlet.io\n- timeout: 50\n+ timeout: 20\n- proxy: 123.234.53.22\n- follow: false\n+ verbose: true\n';
-  expect(genDiff(path1, path2)).toBe(resultFromJson);
+  expect(genDiff(getPath(before), getPath(after))).toBe(resultFromJson);
 });
 
 test('YML', () => {
-  currentPath1 = 'fixtures/yml/before.yml';
-  currentPath2 = 'fixtures/yml/after.yml';
-  path1 = path.join(pwd, currentPath1);
-  path2 = path.join(pwd, currentPath2);
+  before = 'fixtures/yml/before.yml';
+  after = 'fixtures/yml/after.yml';
 
   const resultFromYml = '  format: yml\n- timeout: 100\n+ timeout: 120\n- revision: v1\n+ revision: v2\n+ step: 5\n';
-  expect(genDiff(path1, path2)).toBe(resultFromYml);
+  expect(genDiff(getPath(before), getPath(after))).toBe(resultFromYml);
 });
 
 test('INI', () => {
-  currentPath1 = 'fixtures/ini/before.ini';
-  currentPath2 = 'fixtures/ini/after.ini';
-  path1 = path.join(pwd, currentPath1);
-  path2 = path.join(pwd, currentPath2);
+  before = 'fixtures/ini/before.ini';
+  after = 'fixtures/ini/after.ini';
 
   const resultFromIni = '  car: lexus\n- model: r300\n+ model: r330\n  year: 2004\n- engine: 3.0\n+ engine: 3.3\n- mileage: 167000\n+ mileage: 235000\n';
-  expect(genDiff(path1, path2)).toBe(resultFromIni);
+  expect(genDiff(getPath(before), getPath(after))).toBe(resultFromIni);
 });
 
 test('YML - JSON', () => {
-  currentPath1 = 'fixtures/yml/before.yml';
-  currentPath2 = 'fixtures/json/after.json';
-  path1 = path.join(pwd, currentPath1);
-  path2 = path.join(pwd, currentPath2);
+  before = 'fixtures/yml/before.yml';
+  after = 'fixtures/json/after.json';
 
   const resultYmlJson = '- format: yml\n- timeout: 100\n+ timeout: 20\n- revision: v1\n+ verbose: true\n+ host: hexlet.io\n';
-  expect(genDiff(path1, path2)).toBe(resultYmlJson);
+  expect(genDiff(getPath(before), getPath(after))).toBe(resultYmlJson);
 });
