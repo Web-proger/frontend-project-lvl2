5f588ef3f84273d364e82bbb5caedc1d8ff5e4f9
Ivan Tarasov <guser999@mail.ru>
2020-05-16 14:06:42 +0700

    test(gendiff.test.js) refactor test

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,45 +1,17 @@
-import { test, expect, beforeAll } from '@jest/globals';
+import { test, expect, describe } from '@jest/globals';
 import path from 'path';
 import genDiff from '../src/gendiff';
 
-let pwd;
-let before;
-let after;
-
+const pwd = process.cwd();;
 const getPath = (currentPath) => path.join(pwd, currentPath);
 
-beforeAll(() => {
-  pwd = process.cwd();
-});
-
-test('JSON', () => {
-  before = 'fixtures/json/before.json';
-  after = 'fixtures/json/after.json';
-
-  const resultFromJson = '  host: hexlet.io\n- timeout: 50\n+ timeout: 20\n- proxy: 123.234.53.22\n- follow: false\n+ verbose: true\n';
-  expect(genDiff(getPath(before), getPath(after))).toBe(resultFromJson);
-});
-
-test('YML', () => {
-  before = 'fixtures/yml/before.yml';
-  after = 'fixtures/yml/after.yml';
-
-  const resultFromYml = '  format: yml\n- timeout: 100\n+ timeout: 120\n- revision: v1\n+ revision: v2\n+ step: 5\n';
-  expect(genDiff(getPath(before), getPath(after))).toBe(resultFromYml);
-});
-
-test('INI', () => {
-  before = 'fixtures/ini/before.ini';
-  after = 'fixtures/ini/after.ini';
-
-  const resultFromIni = '  car: lexus\n- model: r300\n+ model: r330\n  year: 2004\n- engine: 3.0\n+ engine: 3.3\n- mileage: 167000\n+ mileage: 235000\n';
-  expect(genDiff(getPath(before), getPath(after))).toBe(resultFromIni);
-});
-
-test('YML - JSON', () => {
-  before = 'fixtures/yml/before.yml';
-  after = 'fixtures/json/after.json';
-
-  const resultYmlJson = '- format: yml\n- timeout: 100\n+ timeout: 20\n- revision: v1\n+ verbose: true\n+ host: hexlet.io\n';
-  expect(genDiff(getPath(before), getPath(after))).toBe(resultYmlJson);
+describe.each([
+  ['JSON', 'fixtures/json/before.json', 'fixtures/json/after.json', '  host: hexlet.io\n- timeout: 50\n+ timeout: 20\n- proxy: 123.234.53.22\n- follow: false\n+ verbose: true\n'],
+  ['YML', 'fixtures/yml/before.yml', 'fixtures/yml/after.yml', '  format: yml\n- timeout: 100\n+ timeout: 120\n- revision: v1\n+ revision: v2\n+ step: 5\n'],
+  ['INI', 'fixtures/ini/before.ini', 'fixtures/ini/after.ini', '  car: lexus\n- model: r300\n+ model: r330\n  year: 2004\n- engine: 3.0\n+ engine: 3.3\n- mileage: 167000\n+ mileage: 235000\n'],
+  ['YML-JSON', 'fixtures/yml/before.yml', 'fixtures/json/after.json', '- format: yml\n- timeout: 100\n+ timeout: 20\n- revision: v1\n+ verbose: true\n+ host: hexlet.io\n'],
+])('config diff', (name, before, after, result) => {
+  test(name, () => {
+    expect(genDiff(getPath(before), getPath(after))).toBe(result);
+  });
 });
