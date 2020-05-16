c46b7a06dbc004c1e0b77e740d8bceac661e5c69
Ivan Tarasov <guser999@mail.ru>
2020-05-16 17:23:11 +0700

    test(gendiff.test.js) добавил тест и ожидаемый результат для дерева

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -11,6 +11,7 @@ describe.each([
   ['JSON', 'fixtures/json/before.json', 'fixtures/json/after.json', 'fixtures/json/resultJsonDiff'],
   ['YML', 'fixtures/yml/before.yml', 'fixtures/yml/after.yml', 'fixtures/yml/resultYmlDiff'],
   ['INI', 'fixtures/ini/before.ini', 'fixtures/ini/after.ini', 'fixtures/ini/resultIniDiff'],
+  ['JSON', 'fixtures/json/treeBefore.json', 'fixtures/json/treeAfter.json', 'fixtures/json/treeJsonDiff'],
 ])('config diff', (name, before, after, result) => {
   test(name, () => {
     expect(genDiff(getPath(before), getPath(after))).toBe(getResult(getPath(result)));
