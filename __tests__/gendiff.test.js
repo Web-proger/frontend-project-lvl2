e387e7e2f6f4d6a4342fcc3cc178426b7bff9010
Ivan Tarasov <guser999@mail.ru>
2020-05-16 12:26:37 +0700

    test(package.json) сделал тесты для ini

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -6,12 +6,12 @@ test('genDiff', () => {
   const pwd = process.cwd();
   let currentPath1 = 'fixtures/json/before.json';
   let currentPath2 = 'fixtures/json/after.json';
-
   let path1 = path.join(pwd, currentPath1);
   let path2 = path.join(pwd, currentPath2);
 
   const resultFromJson = '  host: hexlet.io\n- timeout: 50\n+ timeout: 20\n- proxy: 123.234.53.22\n- follow: false\n+ verbose: true\n';
   expect(genDiff(path1, path2)).toBe(resultFromJson);
+
 
   currentPath1 = 'fixtures/yml/before.yml';
   currentPath2 = 'fixtures/yml/after.yml';
@@ -21,6 +21,7 @@ test('genDiff', () => {
   const resultFromYml = '  format: yml\n- timeout: 100\n+ timeout: 120\n- revision: v1\n+ revision: v2\n+ step: 5\n';
   expect(genDiff(path1, path2)).toBe(resultFromYml);
 
+
   currentPath1 = 'fixtures/yml/before.yml';
   currentPath2 = 'fixtures/json/after.json';
   path1 = path.join(pwd, currentPath1);
@@ -28,4 +29,13 @@ test('genDiff', () => {
 
   const resultYmlJson = '- format: yml\n- timeout: 100\n+ timeout: 20\n- revision: v1\n+ verbose: true\n+ host: hexlet.io\n';
   expect(genDiff(path1, path2)).toBe(resultYmlJson);
+
+
+  currentPath1 = 'fixtures/ini/before.ini';
+  currentPath2 = 'fixtures/ini/after.ini';
+  path1 = path.join(pwd, currentPath1);
+  path2 = path.join(pwd, currentPath2);
+
+  const resultFromIni = '  car: lexus\n- model: r300\n+ model: r330\n  year: 2004\n- engine: 3.0\n+ engine: 3.3\n- mileage: 167000\n+ mileage: 235000\n';
+  expect(genDiff(path1, path2)).toBe(resultFromIni);
 });
