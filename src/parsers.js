57249ecf58a362bbfbd6261b61721c3e745396d6
Ivan Tarasov <guser999@mail.ru>
2020-05-16 12:29:17 +0700

    feat(parsers.json) добавил парсер ini

--- src/parsers.js
+++ src/parsers.js
@@ -1,8 +1,13 @@
 import yaml from 'js-yaml';
+import ini from 'ini';
 
 export default (data, ext) => {
   if (ext === '.yml') {
     return yaml.safeLoad(data);
+  }
+
+  if (ext === '.ini') {
+    return ini.parse(data);
   }
 
   return JSON.parse(data);
