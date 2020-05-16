060e6ef144fcc59296d1c5853018a58f9b8fbfeb
Ivan Tarasov <guser999@mail.ru>
2020-05-16 17:58:31 +0700

    refactor(index.js) вынес работу парсера в index.js, подключил неообходимые зависимости

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 #!/usr/bin/env node
 import { program } from 'commander';
 import path from 'path';
+import fs from 'fs';
 import genDiff from './gendiff';
+import parsers from './parsers';
 
 program
   .version('0.0.1')
@@ -13,9 +15,13 @@ program.parse(process.argv);
 
 const currentPath = process.cwd();
 const [, , file1, file2] = process.argv;
+
 const path1 = path.isAbsolute(file1) ? file1 : path.join(currentPath, file1);
 const path2 = path.isAbsolute(file2) ? file2 : path.join(currentPath, file2);
 
-const diff = genDiff(path1, path2);
+const dataBefore = parsers(fs.readFileSync(path1, 'utf8'), path.extname(path1));
+const dataAfter = parsers(fs.readFileSync(path2, 'utf8'), path.extname(path2));
+
+const diff = genDiff(dataBefore, dataAfter);
 
 console.log(diff);
