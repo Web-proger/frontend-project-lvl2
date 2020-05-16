77ec6414f55d479c8057beeb456e68e3ec8e79e4
Ivan Tarasov <guser999@mail.ru>
2020-05-16 18:45:31 +0700

    refactor(gendiff.js) Учитываем, работаем с последним элементом в конфиге или нет

--- src/gendiff.js
+++ src/gendiff.js
@@ -3,26 +3,27 @@ import _ from 'lodash';
 export default (dataBefore, dataAfter) => {
   let diff = '';
 
-  const keys = new Set([...Object.keys(dataBefore), ...Object.keys(dataAfter)]);
+  const keys = [...new Set([...Object.keys(dataBefore), ...Object.keys(dataAfter)])];
 
-  keys.forEach((key) => {
+  keys.forEach((key, i) => {
     const isBefore = _.has(dataBefore, key);
     const isAfter = _.has(dataAfter, key);
     const beforeValue = dataBefore[key];
     const afterValue = dataAfter[key];
+    const isLast = keys.length - 1 === i ? '' : '\n';
 
     if (isBefore && isAfter) {
       if (beforeValue === afterValue) {
-        diff = diff.concat(`  ${key}: ${beforeValue}\n`);
+        diff = diff.concat(`    ${key}: ${beforeValue}${isLast}`);
       } else {
-        diff = diff.concat(`- ${key}: ${beforeValue}\n+ ${key}: ${afterValue}\n`);
+        diff = diff.concat(`  - ${key}: ${beforeValue}\n  + ${key}: ${afterValue}${isLast}`);
       }
     } else if (isBefore) {
-      diff = diff.concat(`- ${key}: ${beforeValue}\n`);
+      diff = diff.concat(`  - ${key}: ${beforeValue}${isLast}`);
     } else {
-      diff = diff.concat(`+ ${key}: ${afterValue}\n`);
+      diff = diff.concat(`  + ${key}: ${afterValue}${isLast}`);
     }
   });
 
-  return diff;
+  return `{\n${diff}\n}`;
 };
