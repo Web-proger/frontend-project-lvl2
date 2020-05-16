532af00ad2dfe56bd3c27579f1a8b04e2dc82f5d
Ivan Tarasov <guser999@mail.ru>
2020-05-16 16:58:44 +0700

    test(gendiff.test.js) перевёл на проверку чрезультатов через файл фикстур

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,17 +1,18 @@
 import { test, expect, describe } from '@jest/globals';
+import fs from 'fs';
 import path from 'path';
 import genDiff from '../src/gendiff';
 
-const pwd = process.cwd();;
+const pwd = process.cwd();
 const getPath = (currentPath) => path.join(pwd, currentPath);
+const getResult = (resultPath) => fs.readFileSync(resultPath, 'utf-8');
 
 describe.each([
-  ['JSON', 'fixtures/json/before.json', 'fixtures/json/after.json', '  host: hexlet.io\n- timeout: 50\n+ timeout: 20\n- proxy: 123.234.53.22\n- follow: false\n+ verbose: true\n'],
-  ['YML', 'fixtures/yml/before.yml', 'fixtures/yml/after.yml', '  format: yml\n- timeout: 100\n+ timeout: 120\n- revision: v1\n+ revision: v2\n+ step: 5\n'],
-  ['INI', 'fixtures/ini/before.ini', 'fixtures/ini/after.ini', '  car: lexus\n- model: r300\n+ model: r330\n  year: 2004\n- engine: 3.0\n+ engine: 3.3\n- mileage: 167000\n+ mileage: 235000\n'],
-  ['YML-JSON', 'fixtures/yml/before.yml', 'fixtures/json/after.json', '- format: yml\n- timeout: 100\n+ timeout: 20\n- revision: v1\n+ verbose: true\n+ host: hexlet.io\n'],
+  ['JSON', 'fixtures/json/before.json', 'fixtures/json/after.json', 'fixtures/json/resultJsonDiff'],
+  ['YML', 'fixtures/yml/before.yml', 'fixtures/yml/after.yml', 'fixtures/yml/resultYmlDiff'],
+  ['INI', 'fixtures/ini/before.ini', 'fixtures/ini/after.ini', 'fixtures/ini/resultIniDiff'],
 ])('config diff', (name, before, after, result) => {
   test(name, () => {
-    expect(genDiff(getPath(before), getPath(after))).toBe(result);
+    expect(genDiff(getPath(before), getPath(after))).toBe(getResult(getPath(result)));
   });
 });
