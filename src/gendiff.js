41872bca99bd55d4b3f9c15c4ebde60a1cac4557
Ivan Tarasov <guser999@mail.ru>
2020-05-24 15:46:13 +0700

    feat(gendiff.js) Теперь модуль отвечает за внутреннее представление сравнения, боз подготовки внешнего вида вывода

--- src/gendiff.js
+++ src/gendiff.js
@@ -1,37 +1,34 @@
 import _ from 'lodash';
 
 const data = (dataBefore, dataAfter) => {
-  const diff = '';
-
   const keys = [...new Set([...Object.keys(dataBefore), ...Object.keys(dataAfter)])];
 
-  const structure = keys.map((key) => {
+  return keys.reduce((acc, key) => {
     const isBefore = _.has(dataBefore, key);
     const isAfter = _.has(dataAfter, key);
     const beforeValue = dataBefore[key];
     const afterValue = dataAfter[key];
 
     const item = {
-      name: key,
       available: '',
       equal: null,
     };
 
     if (isBefore && isAfter) {
       if (typeof beforeValue === 'object' && typeof afterValue === 'object') {
-        // return `${key}: ${data(beforeValue, afterValue)}`;
-        return data(beforeValue, afterValue);
+        acc[key] = data(beforeValue, afterValue);
+        return acc;
       }
+
       item.available = 'both';
       item.equal = beforeValue === afterValue;
     } else {
       item.available = isBefore ? 'before' : 'after';
     }
+    acc[key] = item;
 
-    return item;
-  });
-
-  return structure || `{\n${diff}\n}`;
+    return acc;
+  }, {});
 };
 
 export default data;
