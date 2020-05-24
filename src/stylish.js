c2b1bc70f89528991d5dd7bae83104e717651104
Web-proger <guser999@mail.ru>
2020-05-24 23:37:20 +0700

    refactor(stylish.js) Вынес в переменные часть значений, для читаемости

--- src/stylish.js
+++ src/stylish.js
@@ -5,23 +5,25 @@ const getDiff = (dataBefore, dataAfter, structure, depth = 0) => {
   const keys = Object.keys(structure);
 
   keys.forEach((key) => {
-    if (structure[key].available === 'before') {
+    const { available, equal } = structure[key];
+
+    if (available === 'before') {
       diff = diff.concat(`${'    '.repeat(depth)}  - ${key}: ${dataBefore[key]}\n`);
     }
 
-    if (structure[key].available === 'after') {
+    if (available === 'after') {
       diff = diff.concat(`${'    '.repeat(depth)}  + ${key}: ${dataAfter[key]}\n`);
     }
 
-    if (structure[key].available === 'both') {
-      if (structure[key].equal === true) {
+    if (available === 'both') {
+      if (equal === true) {
         diff = diff.concat(`${'    '.repeat(depth + 1)}${key}: ${dataBefore[key]}\n`);
       } else {
         diff = diff.concat(`${'    '.repeat(depth)}  - ${key}: ${dataBefore[key]}\n${'    '.repeat(depth)}  + ${key}: ${dataAfter[key]}\n`);
       }
     }
 
-    if (!structure[key].available && !structure[key].equal) {
+    if (!available && !equal) {
       diff += `${'    '.repeat(depth + 1)}${key}:\n${getDiff(dataBefore[key], dataAfter[key], structure[key], depth + 1)}`;
     }
   });
