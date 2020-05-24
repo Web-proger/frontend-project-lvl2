862fa3a06a0d6dbdafea22394cfc53d50c72fd0a
Web-proger <guser999@mail.ru>
2020-05-25 00:16:10 +0700

    tmp(stylish.js) Вынес в переменные часть значений, для читаемости

--- src/stylish.js
+++ src/stylish.js
@@ -1,4 +1,10 @@
-import _ from 'lodash';
+const getValue = (obj, depth) => {
+  const string = Object.keys(obj).reduce((acc, key) => {
+    const value = typeof obj[key] === 'object' ? getValue(obj[key], depth + 1) : obj[key];
+    return acc.concat(`${'    '.repeat(depth)}    ${key}: ${value}\n`);
+  }, '');
+  return `{\n${string}${'    '.repeat(depth)}}`;
+};
 
 const getDiff = (dataBefore, dataAfter, structure, depth = 0) => {
   let diff = '';
@@ -8,18 +14,22 @@ const getDiff = (dataBefore, dataAfter, structure, depth = 0) => {
     const { available, equal } = structure[key];
 
     if (available === 'before') {
-      diff = diff.concat(`${'    '.repeat(depth)}  - ${key}: ${dataBefore[key]}\n`);
+      const value = typeof dataBefore[key] === 'object' ? getValue(dataBefore[key], depth + 1) : dataBefore[key];
+      diff = diff.concat(`${'    '.repeat(depth)}  - ${key}: ${value}\n`);
     }
 
     if (available === 'after') {
-      diff = diff.concat(`${'    '.repeat(depth)}  + ${key}: ${dataAfter[key]}\n`);
+      const value = typeof dataAfter[key] === 'object' ? getValue(dataAfter[key], depth + 1) : dataAfter[key];
+      diff = diff.concat(`${'    '.repeat(depth)}  + ${key}: ${value}\n`);
     }
 
     if (available === 'both') {
       if (equal === true) {
         diff = diff.concat(`${'    '.repeat(depth + 1)}${key}: ${dataBefore[key]}\n`);
       } else {
-        diff = diff.concat(`${'    '.repeat(depth)}  - ${key}: ${dataBefore[key]}\n${'    '.repeat(depth)}  + ${key}: ${dataAfter[key]}\n`);
+        const valueBefore = typeof dataBefore[key] === 'object' ? getValue(dataBefore[key], depth + 1) : dataBefore[key];
+        const valueAfter = typeof dataAfter[key] === 'object' ? getValue(dataAfter[key], depth + 1) : dataAfter[key];
+        diff = diff.concat(`${'    '.repeat(depth)}  - ${key}: ${valueBefore}\n${'    '.repeat(depth)}  + ${key}: ${valueAfter}\n`);
       }
     }
 
