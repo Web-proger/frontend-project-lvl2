1766edd97e9589a188a3bad892f11a9ed5d9257e
Ivan Tarasov <guser999@mail.ru>
2020-05-25 15:51:05 +0700

    refacrtor(gendiff.js) Перенес работу с аргументами программы в action

--- src/index.js
+++ src/index.js
@@ -8,20 +8,19 @@ import parsers from './parsers';
 program
   .version('0.0.1')
   .description('Compares two configuration files and shows a difference.')
-  .option('-f, --format [type]', 'output format')
-  .arguments('<firstConfig> <secondConfig>');
+  .option('-f, --format [type]', 'output format', 'stylish')
+  .arguments('<firstConfig> <secondConfig>')
+  .action((firstConfig, secondConfig) => {
+    const catalog = process.cwd();
+    const path1 = path.isAbsolute(firstConfig) ? firstConfig : path.join(catalog, firstConfig);
+    const path2 = path.isAbsolute(secondConfig) ? secondConfig : path.join(catalog, secondConfig);
+
+    const dataBefore = parsers(fs.readFileSync(path1, 'utf8'), path.extname(path1));
+    const dataAfter = parsers(fs.readFileSync(path2, 'utf8'), path.extname(path2));
+
+    const diff = genDiff(dataBefore, dataAfter, program.format);
+
+    console.log(diff);
+  });
 
 program.parse(process.argv);
-
-const currentPath = process.cwd();
-const [, , file1, file2] = process.argv;
-
-const path1 = path.isAbsolute(file1) ? file1 : path.join(currentPath, file1);
-const path2 = path.isAbsolute(file2) ? file2 : path.join(currentPath, file2);
-
-const dataBefore = parsers(fs.readFileSync(path1, 'utf8'), path.extname(path1));
-const dataAfter = parsers(fs.readFileSync(path2, 'utf8'), path.extname(path2));
-
-const diff = genDiff(dataBefore, dataAfter);
-
-console.log(diff);
