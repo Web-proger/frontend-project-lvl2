f65998267281b4ed56ad5e96fff1ece56de998de
Ivan Tarasov <guser999@mail.ru>
2020-05-25 12:53:55 +0700

    refactor(gendiff.js) код модуля stylish перенесён в gendiff.js

--- src/gendiff.js
+++ src/gendiff.js
@@ -1,6 +1,6 @@
 import _ from 'lodash';
 
-const data = (dataBefore, dataAfter) => {
+const getStructure = (dataBefore, dataAfter) => {
   const keys = [...new Set([...Object.keys(dataBefore), ...Object.keys(dataAfter)])].sort();
 
   return keys.reduce((acc, key) => {
@@ -16,7 +16,7 @@ const data = (dataBefore, dataAfter) => {
 
     if (isBefore && isAfter) {
       if (typeof beforeValue === 'object' && typeof afterValue === 'object') {
-        acc[key] = data(beforeValue, afterValue);
+        acc[key] = getStructure(beforeValue, afterValue);
         return acc;
       }
 
@@ -31,4 +31,55 @@ const data = (dataBefore, dataAfter) => {
   }, {});
 };
 
-export default data;
+const getValue = (obj, depth) => {
+  const string = Object.keys(obj).reduce((acc, key) => {
+    const value = typeof obj[key] === 'object' ? getValue(obj[key], depth + 1) : obj[key];
+    return acc.concat(`${'    '.repeat(depth)}    ${key}: ${value}\n`);
+  }, '');
+  return `{\n${string}${'    '.repeat(depth)}}`;
+};
+
+const getVision = (dataBefore, dataAfter, structure, depth = 0) => {
+  let diff = '';
+  const keys = Object.keys(structure).sort();
+
+  keys.forEach((key) => {
+    const { available, equal } = structure[key];
+
+    if (available === 'before') {
+      const value = typeof dataBefore[key] === 'object' ? getValue(dataBefore[key], depth + 1) : dataBefore[key];
+      diff = diff.concat(`${'    '.repeat(depth)}  - ${key}: ${value}\n`);
+    }
+
+    if (available === 'after') {
+      const value = typeof dataAfter[key] === 'object' ? getValue(dataAfter[key], depth + 1) : dataAfter[key];
+      diff = diff.concat(`${'    '.repeat(depth)}  + ${key}: ${value}\n`);
+    }
+
+    if (available === 'both') {
+      if (equal === true) {
+        diff = diff.concat(`${'    '.repeat(depth + 1)}${key}: ${dataBefore[key]}\n`);
+      } else {
+        const valueBefore = typeof dataBefore[key] === 'object' ? getValue(dataBefore[key], depth + 1) : dataBefore[key];
+        const valueAfter = typeof dataAfter[key] === 'object' ? getValue(dataAfter[key], depth + 1) : dataAfter[key];
+        diff = diff.concat(`${'    '.repeat(depth)}  - ${key}: ${valueBefore}\n${'    '.repeat(depth)}  + ${key}: ${valueAfter}\n`);
+      }
+    }
+
+    if (!available && !equal) {
+      diff += `${'    '.repeat(depth + 1)}${key}: {\n${getVision(dataBefore[key], dataAfter[key], structure[key], depth + 1)}${'    '.repeat(depth + 1)}}\n`;
+    }
+  });
+
+  return diff;
+};
+
+export default (dataBefore, dataAfter, stylish = 'stylish') => {
+  const structure = getStructure(dataBefore, dataAfter);
+  let diff;
+  if (stylish === 'stylish') {
+    diff = getVision(dataBefore, dataAfter, structure).trimRight();
+    diff = `{\n${diff}\n}`;
+  }
+  return diff;
+};
