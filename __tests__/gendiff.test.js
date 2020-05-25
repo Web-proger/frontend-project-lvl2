ae2bb9f16ccd7eca98822f22a14d6889cba58cb4
Ivan Tarasov <guser999@mail.ru>
2020-05-25 12:08:04 +0700

    test(gendiff.test.js) скорректировал тесты. Подключил библиотеку для визуализации

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -3,6 +3,7 @@ import fs from 'fs';
 import path from 'path';
 import genDiff from '../src/gendiff';
 import parsers from '../src/parsers';
+import stylish from '../src/stylish';
 
 // TODO вынести в отдельный модуль, тестирование parser
 // TODO Сделать общай тест, где будут всё модули за раз
@@ -15,11 +16,14 @@ describe.each([
   ['JSON', '__fixtures__/json/before.json', '__fixtures__/json/after.json', '__fixtures__/json/resultJsonDiff'],
   ['YML', '__fixtures__/yml/before.yml', '__fixtures__/yml/after.yml', '__fixtures__/yml/resultYmlDiff'],
   ['INI', '__fixtures__/ini/before.ini', '__fixtures__/ini/after.ini', '__fixtures__/ini/resultIniDiff'],
+  ['JSON-TREE', '__fixtures__/json/treeBefore.json', '__fixtures__/json/treeAfter.json', '__fixtures__/json/treeJsonDiff'],
 ])('config diff', (name, before, after, result) => {
   test(name, () => {
     const beforeData = getData(getPath(before));
     const afterData = getData(getPath(after));
     const resultData = getResult(getPath(result));
-    expect(genDiff(beforeData, afterData).trim()).toBe(resultData.trim());
+    const internalView = genDiff(beforeData, afterData);
+    const diff = `{\n${stylish(beforeData, afterData, internalView).trimRight()}\n}`;
+    expect(diff).toBe(resultData.trim());
   });
 });
