431441524414668ded4e8f8c005a9209c09535d6
Ivan Tarasov <guser999@mail.ru>
2020-05-26 14:50:05 +0700

    feat(plain.js) Сделал форматер plain и реализовал его выбор через заданный формат

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -1,8 +1,12 @@
 import stylish from './stylish';
+import plain from './plain';
 
 export default (dataBefore, dataAfter, structure, format) => {
   if (format === 'stylish') {
     return stylish(dataBefore, dataAfter, structure);
+  }
+  if (format === 'plain') {
+    return plain(dataBefore, dataAfter, structure);
   }
   return `неизвестный формат "${format}"`;
 };
--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -1,3 +1,44 @@
-export default (dataBefore, dataAfter, structure, key) => {
-  console.log(dataBefore, dataAfter, structure, key);
+const isObject = (obj) => typeof obj === 'object';
+
+const plain = (before, after, structure, name = []) => {
+  let diff = '';
+  const keys = Object.keys(structure).sort();
+
+  keys.forEach((key) => {
+    const { available, equal } = structure[key];
+    const beforeVal = before[key];
+    const afterVal = after[key];
+    const path = name.slice();
+    path.push(key);
+    const pathStr = `'${path.join('.')}'`;
+
+    if (available === 'before') {
+      const value = `Property ${pathStr} was deleted\n`;
+      diff = diff.concat(value);
+    }
+
+    if (available === 'after') {
+      const value = `Property ${pathStr} was added with value: ${isObject(afterVal) ? '[complex value]' : afterVal}`;
+      diff = diff.concat(value);
+    }
+
+    if (available === 'both') {
+      if (equal !== true) {
+        const value = `Property ${pathStr} was changed from: ${isObject(beforeVal) ? '[complex value]' : beforeVal} to ${isObject(afterVal) ? '[complex value]' : afterVal}\n`;
+        diff = diff.concat(value);
+      }
+    }
+
+    if (!available && !equal) {
+      const value = `${plain(beforeVal, afterVal, structure[key], path)}\n`;
+      diff = diff.concat(value);
+    }
+  });
+
+  return diff;
+};
+
+export default (dataBefore, dataAfter, structure) => {
+  const diff = plain(dataBefore, dataAfter, structure);
+  return diff;
 };
