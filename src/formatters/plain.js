cdf4b35bfe08914e5b48455c18177d49e728c7db
Web-proger <guser999@mail.ru>
2020-05-30 23:47:06 +0700

    refactor(plain.js) Переписал на использование reduce, вместо forEach, избавился от let

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -1,37 +1,33 @@
-const isObject = (obj) => typeof obj === 'object';
-const getValue = (value) => (isObject(value) ? '[complex value]' : value);
+const getValue = (value) => (typeof value === 'object' ? '[complex value]' : value);
 
 const plain = (before, after, structure, name = []) => {
-  let diff = '';
   const keys = Object.keys(structure).sort();
 
-  keys.forEach((key) => {
+  const diff = keys.reduce((acc, key) => {
     const { available, equal } = structure[key];
     const beforeVal = before[key];
     const afterVal = after[key];
     const path = name.slice();
     path.push(key);
     const pathStr = `'${path.join('.')}'`;
-    let value = '';
 
     if (available === 'before') {
-      value = `Property ${pathStr} was deleted\n`;
+      return acc.concat(`Property ${pathStr} was deleted\n`);
     }
 
     if (available === 'after') {
-      value = `Property ${pathStr} was added with value: ${getValue(afterVal)}\n`;
+      return acc.concat(`Property ${pathStr} was added with value: ${getValue(afterVal)}\n`);
     }
 
     if (available === 'both' && !equal) {
-      value = `Property ${pathStr} was changed from: ${getValue(beforeVal)} to ${getValue(afterVal)}\n`;
+      return acc.concat(`Property ${pathStr} was changed from: ${getValue(beforeVal)} to ${getValue(afterVal)}\n`);
     }
 
     if (!available && !equal) {
-      value = `${plain(beforeVal, afterVal, structure[key], path)}\n`;
+      return acc.concat(`${plain(beforeVal, afterVal, structure[key], path)}\n`);
     }
-
-    diff = diff.concat(value);
-  });
+    return acc;
+  }, '');
 
   return diff.trim();
 };
