81a8014bd0c6e5d30d9f3a9ec54d3dc7ba7dd6c0
Ivan Tarasov <guser999@mail.ru>
2020-06-04 15:41:01 +0700

    test(gendiff.tst.js) Переделал работу с путями

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,48 +1,48 @@
-import { test, expect, describe } from '@jest/globals';
+import { describe, expect, test } from '@jest/globals';
 import fs from 'fs';
 import path from 'path';
 import genDiff from '../src/gendiff';
-import parsers from '../src/parsers';
 
-// TODO вынести в отдельный модуль, тестирование parser
-// TODO Сделать общиай тест, где будут всё модули за раз
-const getResult = (resultPath) => fs.readFileSync(resultPath, 'utf-8');
-const getData = (filePath) => parsers(fs.readFileSync(filePath, 'utf8'), path.extname(filePath));
+const getAbsPath = (fileName) => path.join(process.cwd(), '__fixtures__', fileName);
+const getResult = (fileName) => {
+  const absPath = getAbsPath(fileName);
+  return fs.readFileSync(absPath, 'utf-8').trim();
+};
 
 describe.each([
-  ['JSON', '__fixtures__/before.json', '__fixtures__/after.json', '__fixtures__/diffStylish'],
-  ['YML', '__fixtures__/before.yml', '__fixtures__/after.yml', '__fixtures__/diffStylish'],
-  ['INI', '__fixtures__/before.ini', '__fixtures__/after.ini', '__fixtures__/diffStylish'],
+  ['JSON', 'before.json', 'after.json', 'diffStylish'],
+  ['YML', 'before.yml', 'after.yml', 'diffStylish'],
+  ['INI', 'before.ini', 'after.ini', 'diffStylish'],
 ])('Stylish', (name, before, after, result) => {
   test(name, () => {
-    const beforeData = getData(path.resolve(before));
-    const afterData = getData(path.resolve(after));
+    const beforeData = getAbsPath(before);
+    const afterData = getAbsPath(after);
     const diff = genDiff(beforeData, afterData);
-    const resultData = getResult(path.resolve(result)).trim();
+    const resultData = getResult(result);
     expect(diff).toBe(resultData);
   });
 });
 
 describe.each([
-  ['Plain', '__fixtures__/before.json', '__fixtures__/after.json', '__fixtures__/diffPlain'],
+  ['Plain', 'before.json', 'after.json', 'diffPlain'],
 ])('Plain', (name, before, after, result) => {
   test(name, () => {
-    const beforeData = getData(path.resolve(before));
-    const afterData = getData(path.resolve(after));
+    const beforeData = getAbsPath(before);
+    const afterData = getAbsPath(after);
     const diff = genDiff(beforeData, afterData, 'plain');
-    const resultData = getResult(path.resolve(result)).trim();
+    const resultData = getResult(result);
     expect(diff).toBe(resultData);
   });
 });
 
 describe.each([
-  ['JSON', '__fixtures__/before.json', '__fixtures__/after.json', '__fixtures__/diffJson'],
+  ['JSON', 'before.json', 'after.json', 'diffJson'],
 ])('json', (name, before, after, result) => {
   test(name, () => {
-    const beforeData = getData(path.resolve(before));
-    const afterData = getData(path.resolve(after));
+    const beforeData = getAbsPath(before);
+    const afterData = getAbsPath(after);
     const diff = genDiff(beforeData, afterData, 'json');
-    const resultData = getResult(path.resolve(result)).trim();
+    const resultData = getResult(result);
     expect(diff).toBe(resultData);
   });
 });
