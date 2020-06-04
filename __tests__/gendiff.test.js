87b85d8f0fde33e926dcbe80a69df7c4e3e31d00
Ivan Tarasov <guser999@mail.ru>
2020-06-04 16:12:15 +0700

    test(gendiff.tst.js) Оптимизация тестов

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,48 +1,21 @@
-import { describe, expect, test } from '@jest/globals';
+import { expect, test } from '@jest/globals';
 import fs from 'fs';
 import path from 'path';
 import genDiff from '../src/gendiff';
 
 const getAbsPath = (fileName) => path.join(process.cwd(), '__fixtures__', fileName);
-const getResult = (fileName) => {
-  const absPath = getAbsPath(fileName);
-  return fs.readFileSync(absPath, 'utf-8').trim();
-};
 
-describe.each([
-  ['JSON', 'before.json', 'after.json', 'diffStylish'],
-  ['YML', 'before.yml', 'after.yml', 'diffStylish'],
-  ['INI', 'before.ini', 'after.ini', 'diffStylish'],
-])('Stylish', (name, before, after, result) => {
-  test(name, () => {
-    const beforeData = getAbsPath(before);
-    const afterData = getAbsPath(after);
-    const diff = genDiff(beforeData, afterData);
-    const resultData = getResult(result);
-    expect(diff).toBe(resultData);
-  });
-});
+test.each([
+  ['stylish', 'JSON', 'before.json', 'after.json', 'diffStylish'],
+  ['stylish', 'YML', 'before.yml', 'after.yml', 'diffStylish'],
+  ['stylish', 'INI', 'before.ini', 'after.ini', 'diffStylish'],
+  ['plain', 'JSON', 'before.json', 'after.json', 'diffPlain'],
+  ['json', 'JSON', 'before.json', 'after.json', 'diffJson'],
+])('[Формат: %s, Тип файла: %s])', (format, dataType, before, after, result) => {
+  const beforeData = getAbsPath(before);
+  const afterData = getAbsPath(after);
+  const resultData = fs.readFileSync(getAbsPath(result), 'utf-8').trim();
+  const diff = genDiff(beforeData, afterData, format);
 
-describe.each([
-  ['Plain', 'before.json', 'after.json', 'diffPlain'],
-])('Plain', (name, before, after, result) => {
-  test(name, () => {
-    const beforeData = getAbsPath(before);
-    const afterData = getAbsPath(after);
-    const diff = genDiff(beforeData, afterData, 'plain');
-    const resultData = getResult(result);
-    expect(diff).toBe(resultData);
-  });
-});
-
-describe.each([
-  ['JSON', 'before.json', 'after.json', 'diffJson'],
-])('json', (name, before, after, result) => {
-  test(name, () => {
-    const beforeData = getAbsPath(before);
-    const afterData = getAbsPath(after);
-    const diff = genDiff(beforeData, afterData, 'json');
-    const resultData = getResult(result);
-    expect(diff).toBe(resultData);
-  });
+  expect(diff).toBe(resultData);
 });
