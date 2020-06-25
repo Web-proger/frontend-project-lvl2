ae0897e7ac4f5425f8452ba4785806167a2fc318
Web-proger <guser999@mail.ru>
2020-06-25 09:39:25 +0700

    refactor (json.js) перевод json на работу с новой структурой

--- src/formatters/json.js
+++ src/formatters/json.js
@@ -2,15 +2,15 @@ import _ from 'lodash';
 
 const getValue = (value) => (_.isObject(value) ? { ...value } : value);
 
-const json = (dataBefore, dataAfter, structure, keys = []) => structure
+const json = (structure, keys = []) => structure
   .reduce((acc, {
     key,
     available,
     equal,
     children,
+    beforeValue,
+    afterValue,
   }) => {
-    const beforeValue = dataBefore[key];
-    const afterValue = dataAfter[key];
     const path = [...keys, key];
 
     const setData = (status, oldValue, newValue) => {
@@ -34,14 +34,14 @@ const json = (dataBefore, dataAfter, structure, keys = []) => structure
     }
 
     if (children.length > 0) {
-      const obj = json(beforeValue, afterValue, children, keys);
+      const obj = json(children, keys);
       _.set(acc, path, obj);
     }
 
     return acc;
   }, {});
 
-export default (bef, aft, str) => {
-  const diff = json(bef, aft, str);
+export default (structure) => {
+  const diff = json(structure);
   return JSON.stringify(diff);
 };
