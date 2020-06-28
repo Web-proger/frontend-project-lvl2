fe379da605f6a2ed8fffc2f0f0491e1144fa3d05
Ivan Tarasov <guser999@mail.ru>
2020-06-28 14:00:52 +0700

    refactor(parsers.js) Переделал на switch case

--- src/parsers.js
+++ src/parsers.js
@@ -2,17 +2,14 @@ import yaml from 'js-yaml';
 import ini from 'ini';
 
 export default (data, type) => {
-  if (type === 'yml') {
-    return yaml.safeLoad(data);
+  switch (type) {
+    case 'yml':
+      return yaml.safeLoad(data);
+    case 'ini':
+      return ini.parse(data);
+    case 'json':
+      return JSON.parse(data);
+    default:
+      throw new Error(`Unknown data format "${type}"`);
   }
-
-  if (type === 'ini') {
-    return ini.parse(data);
-  }
-
-  if (type === 'json') {
-    return JSON.parse(data);
-  }
-
-  throw new Error(`Unknown data format "${type}"`);
 };
