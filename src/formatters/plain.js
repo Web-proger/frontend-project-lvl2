6b97e737183c080d7337ccacaf7e35c214971d1c
Ivan Tarasov <guser999@mail.ru>
2020-07-01 17:08:28 +0700

    refactor(index.js) Скрыл параметры для внутренних нужд

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -2,32 +2,35 @@ import _ from 'lodash';
 
 const getValue = (value) => (_.isObject(value) ? '[complex value]' : value);
 
-const plain = (structure, keys = []) => structure
-  .flatMap(({
-    key,
-    status,
-    children,
-    beforeValue,
-    afterValue,
-  }) => {
-    const pathParts = [...keys, key];
-    const path = `'${pathParts.join('.')}'`;
+const plain = (structure) => {
+  const iter = (innerStructure, keys) => innerStructure
+    .flatMap(({
+      key,
+      status,
+      children,
+      beforeValue,
+      afterValue,
+    }) => {
+      const pathParts = [...keys, key];
+      const path = `'${pathParts.join('.')}'`;
 
-    if (children.length > 0) {
-      return `${plain(children, pathParts)}`;
-    }
+      if (children.length > 0) {
+        return `${iter(children, pathParts)}`;
+      }
 
-    switch (status) {
-      case 'deleted':
-        return `Property ${path} was deleted`;
-      case 'added':
-        return `Property ${path} was added with value: ${getValue(afterValue)}`;
-      case 'modified':
-        return `Property ${path} was changed from: ${getValue(beforeValue)} to ${getValue(afterValue)}`;
-      default:
-        return [];
-    }
-  }).join('\n');
+      switch (status) {
+        case 'deleted':
+          return `Property ${path} was deleted`;
+        case 'added':
+          return `Property ${path} was added with value: ${getValue(afterValue)}`;
+        case 'modified':
+          return `Property ${path} was changed from: ${getValue(beforeValue)} to ${getValue(afterValue)}`;
+        default:
+          return [];
+      }
+    }).join('\n');
 
+  return iter(structure, []);
+};
 
 export default plain;
