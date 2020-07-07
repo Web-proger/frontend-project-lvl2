6228c322d02c9cdbe9da985ccc7cf2207fadf585
Web-proger <guser999@mail.ru>
2020-07-07 09:35:02 +0700

    refactor(index.js) промежуточный вариант структуры

--- src/index.js
+++ src/index.js
@@ -5,62 +5,72 @@ import formatters from './formatters';
 import parse from './parsers';
 import util from 'util';
 
-const getStructure = (node) => Object.entries(node).map(([key, value]) => {
-  const type = _.isObject(value) ? 'composite' : 'simple';
-  const children = _.isObject(value) ? getStructure(value) : [];
-
-  if (type === 'composite') {
-    return {
-      key,
-      type,
-      children,
-    };
-  }
-
-  return {
-    key,
-    type,
-    value,
-  };
-});
-
 const getDiffStructure = (dataBefore, dataAfter) => {
   const keys = _.union(_.keys(dataBefore), _.keys(dataAfter)).sort();
+  const getType = (value) => (_.isObject(value) ? 'composite' : 'simple');
 
   return keys.map((key) => {
     const beforeValue = dataBefore[key];
     const afterValue = dataAfter[key];
+    const beforeType = getType(beforeValue);
+    const afterType = getType(afterValue);
     const hasKeyBoth = _.has(dataBefore, key) && _.has(dataAfter, key);
     const valueEqual = hasKeyBoth && (beforeValue === afterValue);
+    const bothObject = _.isObject(beforeValue) && _.isObject(afterValue);
 
-
-    if (hasKeyBoth) {
+    if (bothObject) {
+      const children = getDiffStructure(beforeValue, afterValue);
       return {
         key,
-        status: valueEqual ? 'unmodified' : 'modified',
-        nodeBefore: {
-          type: beforeType,
-          value: beforeValue,
-          children,
-        },
-        nodeAfter: {
-          type: afterType,
-          value: afterValue,
-          children,
-        },
+        type: 'composite',
+        children,
       };
     }
 
     if (valueEqual) {
+      return {
+        key,
+        type: 'simple',
+        status: 'unmodified',
+        nodeBefore: {
+          type: beforeType,
+          value: beforeValue,
+        },
+        nodeAfter: {
+          type: afterType,
+          value: afterValue,
+        },
+      };
+    }
 
+    if (hasKeyBoth) {
+      return {
+        key,
+        type: 'simple',
+        status: 'modified',
+        nodeBefore: {
+          type: beforeType,
+          value: beforeValue,
+        },
+        nodeAfter: {
+          type: afterType,
+          value: afterValue,
+        },
+      };
     }
 
     return {
       key,
       type: 'simple',
       status: beforeValue ? 'deleted' : 'added',
-      beforeValue,
-      afterValue,
+      nodeBefore: {
+        type: beforeType,
+        value: beforeValue,
+      },
+      nodeAfter: {
+        type: afterType,
+        value: afterValue,
+      },
     };
   });
 };
@@ -77,15 +87,11 @@ export default (firstConfig, secondConfig, formatType = 'stylish') => {
   const oldData = parse(fs.readFileSync(oldFilepath, 'utf8'), oldExtname);
   const newData = parse(fs.readFileSync(newFilepath, 'utf8'), newExtname);
 
-  const beforeStructure = getStructure(oldData);
-  const afterStructure = getStructure(newData);
-
   const format = formatters(formatType);
-  //const diff = getDiffStructure(beforeStructure, afterStructure);
+  const diff = getDiffStructure(oldData, newData);
 
   //return format(diff);
 
 
-  console.log(util.inspect(beforeStructure, { depth: 20 }));
-  console.log(util.inspect(afterStructure, { depth: 20 }));
+  console.log(util.inspect(diff, { depth: 20 }));
 };
